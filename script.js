061059029e7efb07772ead0dfffc903994245bf7
ExBoyFriends <tomboy.maker@gmail.com>
2026-01-15 17:44:29 +0900

    script.js を更新

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const book = document.querySelector(".book");
 
+window.addEventListener("deviceorientation", (e) => {
+  const x = e.gamma || 0; // 左右
+  const y = e.beta || 0;  // 前後
+
+  const rotateY = x * 0.2;
+  const rotateX = y * 0.1;
+
+  book.style.transform = `
+    rotateX(${-rotateX}deg)
+    rotateY(${rotateY}deg)
+  `;
+});
